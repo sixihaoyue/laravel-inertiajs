aebb99a872f74867076c832d13608d39a219f718
sixihaoyue <sixihaoyue@gmail.com>
2023-01-13 17:00:43 +0800

    优化ssr命令

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,32 +1,34 @@
 const mix = require('laravel-mix');
 const path = require('path');
-require('laravel-mix-clean');
+//
+const cssModuleIdentifier = '[name]__[local]--[hash:base64:5]';
+const alias =  {
+  'resources': path.resolve('resources')
+}
+if (process.env.npm_config_ssr) {
+  const nodeExternals = require('webpack-node-externals');
+  mix.options({ cssModuleIdentifier, manifest: false })
+    .ts('resources/js/ssr.tsx', 'public/ssr/app.js')
+    .react({extractStyles: 'public/ssr/app.css'})
+    .webpackConfig({
+      target: 'node',
+      resolve: { alias },
+      externals: [nodeExternals()],
+    })
+} else {
+  require('laravel-mix-clean');
+  mix.extract();
+  mix.options({ cssModuleIdentifier })
+    .ts('resources/js/app.tsx', 'build/js')
+    .react({extractStyles: 'build/css/app.css'})
+    .webpackConfig({
+      output: { chunkFilename: 'build/js/[name].js?id=[chunkhash]' },
+      resolve: { alias },
+    })
+    .clean({
+      cleanOnceBeforeBuildPatterns: [path.resolve(__dirname, 'public/build/**/*')],
+    })
+    .version()
+    .sourceMaps();
+}
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.extract();
-mix.options({ 'cssModuleIdentifier': '[name]__[local]--[hash:base64:5]' })
-  .ts('resources/js/app.tsx', 'build/js')
-  .react({extractStyles: 'build/css/app.css'})
-  .webpackConfig({
-    output: { chunkFilename: 'build/js/[name].js?id=[chunkhash]' },
-    resolve: {
-      alias: {
-        'resources': path.resolve('resources')
-      }
-    },
-  })
-  .clean({
-    cleanOnceBeforeBuildPatterns: [path.resolve(__dirname, 'public/build/**/*')],
-   })
-  .version()
-  .sourceMaps();
